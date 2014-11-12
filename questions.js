27635e8eaa60e5593fac68b0f7b38293f20f84b6
Kseniya Redunova <redunova@hotmail.com>
2014-11-12 16:58:35 +0200

    Updated code to new versions of angular, firebase and angularfire; added example of the firebase json - txt.json

--- questions.js
+++ questions.js
@@ -1,18 +1,18 @@
-﻿angular.module('question', ['firebase']).
-	value('fbURL', 'https://ksenia-q-a-firebase.firebaseio.com/').
-	factory('Question', function(angularFireCollection, fbURL) {
-		return angularFireCollection(fbURL);
-	}).
-	config(function($routeProvider) {
-		$routeProvider.
-			when('/', {controller:ListCtrl, templateUrl:'list.html'}).
-			when('/question/:questionId', {controller:EditCtrl, templateUrl:'question.html'}).
-			when('/edit/:questionId', {controller:EditCtrl, templateUrl:'detail.html'}).
-			when('/new', {controller:CreateCtrl, templateUrl:'detail.html'}).
-			otherwise({redirectTo:'/'});
-	});
+﻿angular.module('question', ['ngRoute', 'firebase'])
+	.constant('fbURL', 'https://amber-fire-2430.firebaseio.com/')
+	.factory('Question', function ($firebase, fbURL) {
+		return $firebase(new Firebase(fbURL)).$asArray();
+	})
+	.config(['$routeProvider', function($routeProvider) {
+		$routeProvider
+			.when('/', {controller:ListController, templateUrl:'list.html'})
+			.when('/question/:questionId', {controller:EditController, templateUrl:'question.html'})
+			.when('/edit/:questionId', {controller:EditController, templateUrl:'detail.html'})
+			.when('/new', {controller:CreateController, templateUrl:'detail.html'})
+			.otherwise({redirectTo:'/'});
+	}]);
  
-function ListCtrl($scope, Question) {
+function ListController($scope, Question) {
 	$scope.question = Question;
 	$scope.showFilter = 'all';
 	
@@ -21,45 +21,52 @@ function ListCtrl($scope, Question) {
 	};
 }
  
-function CreateCtrl($scope, $location, $timeout, Question) {
+function CreateController($scope, $location, $timeout, Question) {
 	$scope.save = function() {
 		$scope.question.answered = false;
-		Question.add($scope.question, function() {
-			$timeout(function() { $location.path('/'); });
-		});
+		Question.$add($scope.question)
+			.then(function(ref) {
+				$location.path('/');
+			});
 	}
 }
  
-function EditCtrl($scope, $location, $routeParams, angularFire, fbURL) {
-	angularFire(fbURL + $routeParams.questionId, $scope, 'remote', {}).
-	then(function() {
-		$scope.question = angular.copy($scope.remote);
-		$scope.question.$id = $routeParams.questionId;
-		$scope.isClean = function() {
-			return angular.equals($scope.remote, $scope.question);
-		}
-		$scope.destroy = function() {
-			$scope.remote = null;
-			$location.path('/');
-		};
-		$scope.save = function() {
-			$scope.remote = angular.copy($scope.question);
-			$location.path('/');
-		};
-		$scope.saveAnswer = function() {
-			$scope.question.answered = true;
-			if (!$scope.question.answers || !angular.isArray($scope.question.answers)) {
-				$scope.question.answers = [];
-			}
-			$scope.question.answers.push({
-				author: $scope.author,
-				text: $scope.text
-			});
-			$scope.author = '';
-			$scope.text = '';
+function EditController($scope, $location, $routeParams, fbURL, $firebase) {
+	var sync = $firebase(new Firebase(fbURL + $routeParams.questionId));
+	$scope.question = sync.$asObject();
+	$scope.question.$id = $routeParams.questionId;
+	
+	$scope.isClean = function() {
+		return angular.equals($scope.remote, $scope.question);
+	}
 
-			$scope.remote = angular.copy($scope.question);
+	$scope.destroy = function() {
+		var syncAll = $firebase(new Firebase(fbURL)).$asArray();
+		syncAll.$loaded().then(function(data) {
+			syncAll.$remove(syncAll.$getRecord($routeParams.questionId));
 			$location.path('/');
-		};
-	});
+		})
+		
+	}
+
+	$scope.save = function() {
+		$scope.question.$save();
+		$location.path('/');
+	}
+
+	$scope.saveAnswer = function() {
+		$scope.question.answered = true;
+		if (!$scope.question.answers || !angular.isArray($scope.question.answers)) {
+			$scope.question.answers = [];
+		}
+		$scope.question.answers.push({
+			author: $scope.author,
+			text: $scope.text
+		});
+		$scope.author = '';
+		$scope.text = '';
+
+		$scope.question.$save();
+		$location.path('/');
+	};
 }
